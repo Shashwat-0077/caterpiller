f604a1edd6493e87e094ffb08a8e916289252433
Shashwat Gupta <shashwatg0911@gmail.com>
2024-08-10 15:06:54 +0530

    Build : created the navbar

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,5 +3,17 @@ import { auth } from "@/auth";
 export default async function Dashboard() {
     const session = await auth();
 
-    return <div>{JSON.stringify(session)}</div>;
+    return (
+        <div>
+            <p>
+                navbar Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Architecto ex dicta reprehenderit veniam quo reiciendis ea
+                inventore asperiores, ad culpa excepturi doloribus modi iste,
+                cum adipisci doloremque aspernatur laborum aut autem nam
+                voluptatum corporis velit sequi suscipit! Temporibus rerum,
+                dolores non ipsum ad qui! Voluptatibus ratione explicabo
+                excepturi sed cupiditate!
+            </p>
+        </div>
+    );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
-import Navbar from "@/components/Navbar";
 import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,10 +19,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={inter.className}>
-                <SessionProvider>
-                    <Navbar />
-                    {children}
-                </SessionProvider>
+                <SessionProvider>{children}</SessionProvider>
             </body>
         </html>
     );
